ab74bcc187b558077ae3d0d527809dba3a00b76b
HeitorDalla <heitorvillavilla@gmail.com>
2025-06-05 15:04:46 -0300

    Filtros finalizados

--- main.py
+++ main.py
@@ -13,3 +13,56 @@ st.set_page_config(
     layout="wide"
 )
 
+df = pd.read_csv("data/dados_vendas.csv", sep=',', encoding='utf-8', parse_dates=['data_venda'])
+
+st.header("🍨 Dashboard Sorveteria - Análise de Vendas")
+
+
+# Filtros laterais para melhor organização
+
+# Filtros de data
+filtro_data_inicio = st.sidebar.date_input(
+    'Data início',
+    value = df['data_venda'].min().date(), # Pega o valor mais antigo da coluna data_venda e o transforma em data, sem horario
+    min_value = df['data_venda'].min().date(),
+    max_value = df['data_venda'].max().date()
+)
+
+filtro_data_fim = st.sidebar.date_input(
+    'Data fim',
+    value = df['data_venda'].max().date(),
+    min_value = df['data_venda'].min().date(),
+    max_value = df['data_venda'].max().date()
+)
+
+# Filtro por categoria
+categorias = df['categoria'].unique()
+categoriasLista = ['Todas'] + list(categorias)
+categoria_selecionada = st.sidebar.selectbox("Categorias", categoriasLista)
+
+# Filtros por pagamentos
+pagamentos = df['forma_pagamento'].unique()
+pagamentosLista = ['Todos'] + list(pagamentos)
+pagamento_selecionado = st.sidebar.selectbox("Pagamentos", pagamentosLista)
+
+# Filtros por clientes
+clientes = df['cliente'].unique()
+clienteLista = ['Todos'] + list(clientes)
+cliente_selecionado = st.sidebar.selectbox("Clientes", clienteLista)
+
+# Filtro total
+df_filtrado = df[
+    (df['data_venda'].dt.date >= filtro_data_inicio) & 
+    (df['data_venda'].dt.date <= filtro_data_fim)
+]
+
+if categoria_selecionada != 'Todas': # o usuário clicou em uma das opções
+    df_filtrado = df_filtrado[df_filtrado['categoria'] == categoria_selecionada]
+
+if pagamento_selecionado != 'Todos': # o usuário clicou em uma das opções
+    df_filtrado = df_filtrado[df_filtrado['forma_pagamento'] == pagamento_selecionado]
+
+if cliente_selecionado != 'Todos': # o usuário clicou em uma das opções
+    df_filtrado = df_filtrado[df_filtrado['cliente'] == cliente_selecionado]
+
+
